0dcdff126092902faf163457ad2a24ac8439f04b
juli6464 <juli6464@hotmail.com>
2023-08-17 09:58:44 -0500

    creando servicios

---
+++ src/services/products.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from './../entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private counterId = 1;
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'product 1',
+      description: 'camisa blanca',
+      price: 122,
+      image: '',
+      stock: 3,
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  create(payload: any) {
+    this.counterId = this.counterId + 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  update(id: number, payload: any) {
+    const productFound = this.products.findIndex((item) => item.id === id);
+    let message = '';
+    if (productFound > 0) {
+      this.products[productFound] = {
+        id: id,
+        ...payload,
+      };
+      message = 'Product updated';
+    } else {
+      message = 'Product not found';
+    }
+    return message;
+  }
+
+  delete(id: number) {
+    const productFound = this.products.findIndex((item) => item.id === id);
+    let message = '';
+    if (productFound > 0) {
+      this.products.splice(productFound, 1);
+      message = 'product deleted';
+    } else {
+      message = 'product not found';
+    }
+    return message;
+  }
+}
